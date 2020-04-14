56c8e35c2819c624755a0b13d3e60a4d2443f802
Oleksii (Alex) <47008713+Alexbasisty@users.noreply.github.com>
2020-04-14 16:04:51 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+const initialState = {
+    a: 0,
+    b: 0,
+    result: 0,
+};
+
+reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "NewAValue":
+            return { ...state, a: Number(action.newValue) };
+        case "NewBValue":
+            return { ...state, b: Number(action.newValue) };
+        case "Addition":
+            return { ...state, result: state.a + state.b };
+        case "Multiplication":
+            return { ...state, result: state.a * state.b };
+        default:
+            console.log(`Unknown action ${action.type}`);
+            return state;
+    }
+};
+
+const store = Redux.createStore(reducer);
+
+const inputA = document.querySelector("#first");
+const inputB = document.querySelector("#second");
+
+const buttonAdd = document.querySelector("#add");
+const buttonMultiply = document.querySelector("#multiply");
+
+handleAUpdate = (event) => {
+    const updateAction = {
+        type: "NewAValue",
+        newValue: inputA.value,
+    };
+
+    store.dispatch(updateAction);
+};
+handleBUpdate = (event) => {
+    const updateAction = {
+        type: "NewBValue",
+        newValue: inputB.value,
+    };
+
+    store.dispatch(updateAction);
+};
+
+handleAddition = (event) => {
+    const addAction = {
+        type: "Addition",
+    };
+
+    store.dispatch(addAction);
+    console.log(store.getState().result);
+};
+
+handleMultiplication = (event) => {
+    const multiplyAction = {
+        type: "Multiplication",
+    };
+
+    store.dispatch(multiplyAction);
+    console.log(store.getState().result);
+};
+
+inputA.addEventListener("change", handleAUpdate);
+inputB.addEventListener("change", handleBUpdate);
+
+buttonAdd.addEventListener("click", handleAddition);
+buttonMultiply.addEventListener("click", handleMultiplication);
